0d9351771ede462285f0b625dba4e2079aa68fff
Michael Morales <156836539+toxinfang@users.noreply.github.com>
2024-02-19 23:22:18 -0600

    ESPE

--- KrazyCarts/Assets/Scripts/Load Ending.cs
+++ KrazyCarts/Assets/Scripts/Load Ending.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class LoadEnding : MonoBehaviour
 {
+    public Timer timer;
     // Start is called before the first frame update
     void Start()
     {
-
+        timer = GetComponent<Timer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (transform.childCount < 1)
+        {
+            Application.LoadLevel("Win");
+        }
+        else if(timer.timeRemaining == 0)
         {
             Application.LoadLevel("SampleScene");
         }
