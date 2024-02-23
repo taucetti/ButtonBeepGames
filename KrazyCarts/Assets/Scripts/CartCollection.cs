65e8cec48eb8a9c84bfcbad5ee19d6119613c189
Michael Morales <156836539+toxinfang@users.noreply.github.com>
2024-02-22 22:34:27 -0600

    WIP Sound collection

--- KrazyCarts/Assets/Scripts/CartCollection.cs
+++ KrazyCarts/Assets/Scripts/CartCollection.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+
 public class CartCollection : MonoBehaviour
 {
    public int NumberOfCarts { get; private set; }
-
+    public AudioSource soundClip;
+    public AudioClip soundEffect;
     public UnityEvent<CartCollection> OnCartCollected;
     public void CartCollected()
     {
+        soundClip.PlayOneShot(soundEffect);
         NumberOfCarts++;
         OnCartCollected.Invoke(this);
     }
