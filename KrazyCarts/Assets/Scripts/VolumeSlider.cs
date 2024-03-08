1c440bcfbc5ca5ab2b3cdc847fe856b6cfd4e196
nikoxs <nikolaka117@hotmail.com>
2024-03-07 18:17:42 -0600

    added volume slider and resolution settings

---
+++ KrazyCarts/Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+public class VolumeSlider : MonoBehaviour
+{
+    public float volume;   
+    public AudioMixer mixer;
+
+    public void SetVolume(float volume)   // created a function name set volume this will be the name that shows up in unity
+    {
+        mixer.SetFloat("Volume", volume);   // this will reference for my audio mixer
+    }
+
+    public void LowGraphics ()
+    {
+        QualitySettings.SetQualityLevel(0);
+    }
+
+    public void MediumGraphics ()
+    {
+        QualitySettings.SetQualityLevel(1);
+    }
+
+    public void HighGraphics ()
+    {
+        QualitySettings.SetQualityLevel(2);
+    }
+
+}
