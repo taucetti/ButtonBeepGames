bddac95d456623b7e20b128d2ca27fabb983e9fc
Mario Barrera <mbavenger2004@gmail.com>
2024-03-12 16:43:08 -0500

    WIP MP3 Player

---
+++ KrazyCarts/Assets/Scripts/MP3.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Audio;
+
+public class MP3 : MonoBehaviour
+{
+/*    public AudioClip[] musicClips;
+    public AudioSource source;
+    private int currentTrack;*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+/*    public void playMusic()
+    {
+        if (source.isPlaying)
+        {
+            return;
+        }
+
+        currentTrack--;
+        if (currentTrack < 0)
+        {
+            currentTrack = musicClips.Length - 1;
+        }
+    }*/
+
+
+}
