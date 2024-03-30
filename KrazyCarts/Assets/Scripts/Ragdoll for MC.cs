e0d58c2018c1b5accb34b21f2c7ed3051f5f102f
min1227 <evelyn.m.arellano@hotmail.com>
2024-03-30 14:43:45 -0500

    Ragdoll WIP 2
    need to come back after I find out how to fix the waist

--- KrazyCarts/Assets/Scripts/Ragdoll for MC.cs
+++ KrazyCarts/Assets/Scripts/Ragdoll for MC.cs
@@ -1,18 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RagdollforMC : MonoBehaviour
+
 {
-    // Start is called before the first frame update
+    public BoxCollider mainCollider;
+    public GameObject MCRig;
+    //public animator when animations are added
     void Start()
     {
-        
+        GetRagdollParts();
+        RagdollModeOff();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Cars"))
+        {
+            RagdollModeOn();
+
+        }
+
+    }
+
+    Collider[] ragDollColliders;
+    Rigidbody[] limbsRigidBodies;
+
+    void GetRagdollParts()
+    {
+        ragDollColliders = MCRig.GetComponentsInChildren<Collider>();
+        limbsRigidBodies = MCRig.GetComponentsInChildren<Rigidbody>();
+    }
+    void RagdollModeOn()
+    {
+        foreach (Collider col in ragDollColliders)
+        {
+            col.enabled = true;
+        }
+
+        foreach (Rigidbody rigid in limbsRigidBodies)
+        {
+            rigid.isKinematic = false;
+        }
+
+        mainCollider.enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    void RagdollModeOff()
+    {
+        foreach(Collider col in ragDollColliders)
+        {
+            col.enabled = false;
+        }
+
+        foreach(Rigidbody rigid in limbsRigidBodies)
+        {
+            rigid.isKinematic = true;
+        }
+
+        mainCollider.enabled = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+
+
+    }
+
 }
+
+
