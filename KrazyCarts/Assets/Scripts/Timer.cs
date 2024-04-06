8b3aed62c8abcfe396eab13917c738c809e61102
min1227 <evelyn.m.arellano@hotmail.com>
2024-04-05 22:53:24 -0500

    Trying to change Timer

--- KrazyCarts/Assets/Scripts/Timer.cs
+++ KrazyCarts/Assets/Scripts/Timer.cs
@@ -1,43 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; //This gives us access to our UI elements so we can display the text. Only using it internally? Don't need it.
+ //This gives us access to our UI elements so we can display the text. Only using it internally? Don't need it.
 using TMPro;
+using UnityEngine.UI;
 
 //This is a basic timer good for counting down seconds.
 //It counts down as full seconds and stops immediately when you get to 0
 
 public class Timer : MonoBehaviour
 {
-    public float timeRemaining = 120f;
+    public float timeRemaining = 210f;
     public float endTime = 0f;
     public bool timerRunning = false;
     public TextMeshProUGUI timerText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        timerRunning = true;
-    }
 
-    // Update is called once per frame
-    void Update()
+    public class Timer3 : MonoBehaviour
     {
-        if (timerRunning)
+        public float timeRemaining = 210f;
+        public float endTime = 0f;
+        public bool timerRunning = false;
+        public Text timerText;
+
+
+        // Start is called before the first frame update
+        void Start()
         {
-            if (timeRemaining > endTime)
-            {
-                timeRemaining -= Time.deltaTime;
-                timerText.text = ("Time: ") + Mathf.FloorToInt(timeRemaining + 1).ToString();
-            }
-            else
-            {
-                timerRunning = false;
-                timeRemaining = 0;
-                timerText.text = ("Time: ") + Mathf.FloorToInt(timeRemaining).ToString();
-            }
+            timerRunning = true;
         }
 
-        
+        // Update is called once per frame
+        void Update()
+        {
+            if (timerRunning)
+            {
+                if (timeRemaining > endTime)
+                {
+                    timeRemaining -= Time.deltaTime;
+                    DisplayTime(timeRemaining);
+                }
+                else
+                {
+                    timerRunning = false;
+                    timeRemaining = 0;
+                    timerText.text = string.Format("Time Remaining: 0:00");
+                }
+            }
+
+
+
+        }
+
+
+        void DisplayTime(float timeToDisplay)
+        {
+            timeToDisplay += 1;
+            float minutes = Mathf.FloorToInt(timeToDisplay / 60); //divide total by 60, full number is minutes
+            float seconds = Mathf.FloorToInt(timeToDisplay % 60); //divide total by 60, remainder is seconds
+            timerText.text = string.Format("Time Remaining: {0:00}:{1:00}", minutes, seconds);
+        }
     }
 }
