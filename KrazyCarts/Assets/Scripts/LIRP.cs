a562d62e6e01ef5a0cbe6756c1a1b0f001813214
min1227 <evelyn.m.arellano@hotmail.com>
2024-04-07 16:29:48 -0500

    Player Quitting WIP
    I have it working so that the player can press E within a certain range to quit... Can't figure out how to disable text until they are in that range

--- KrazyCarts/Assets/Scripts/LIRP.cs
+++ KrazyCarts/Assets/Scripts/LIRP.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class LIRP : MonoBehaviour
 {
+
+    public Transform a;
+    public Transform b;
+
+    public float moveSpeed;
+    private Transform current;
+    private Transform target;
+    private float sinTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        current = a;
+        target = b;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position != target.position)
+        {
+            sinTime += Time.deltaTime * moveSpeed;
+            sinTime = Mathf.Clamp(sinTime, 0, Mathf.PI);
+            float t = evaluate(sinTime);
+            transform.position = Vector3.Lerp(current.position, target.position, t);
+        }
+
+        swap();
+    }
+
+
+    public void swap()
+    {
+        if (transform.position != target.position)
+        {
+            return;
+        }
+
+        Transform t = current;
+        current = target;
+        target = t;
+        sinTime = 0;
+    }
+    public float evaluate(float x)
+    {
+        return 0.5f * Mathf.Sin(x - Mathf.PI / 2f) + 0.5f;
     }
 }
---
+++ KrazyCarts/Assets/Scripts/Show Quit Text.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ShowQuitText : MonoBehaviour
+{
+
+    public GameObject uiObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == ("Player"))
+            {
+                uiObject.SetActive(true);
+            }
+        uiObject.SetActive(true);
+       
+    }
+   
+}
