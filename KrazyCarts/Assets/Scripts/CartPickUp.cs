541f8cf23c06d4891c1f1c846369fc38fb11196e
Michael Morales <156836539+toxinfang@users.noreply.github.com>
2024-04-14 12:32:11 -0500

    WIP cartpickup

--- KrazyCarts/Assets/Scripts/CartPickUp.cs
+++ KrazyCarts/Assets/Scripts/CartPickUp.cs
@@ -7,10 +7,17 @@ public class CartPickUp : MonoBehaviour
     public CartLine cartLine;
     bool stop = false;
 
+    BoxCollider m_Collider;
+    float m_ScaleX, m_ScaleY, m_ScaleZ;
+
     // Start is called before the first frame update
     void Start()
     {
-     
+        m_Collider = GetComponent<BoxCollider>();
+        m_ScaleX = 1;
+        m_ScaleY = 1;
+        m_ScaleZ = 1;
+
     }
 
     // Update is called once per frame
@@ -24,6 +31,9 @@ public class CartPickUp : MonoBehaviour
             cartLine._PlayerCarts[cartLine.cartCount].SetActive(true);
             cartLine.cartCount++;
             stop = true;
+            m_ScaleZ += 3;
+
+            m_Collider.size = new Vector3(m_ScaleX, m_ScaleY, m_ScaleZ);
         }
         
 
