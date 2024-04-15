5ebaeca542a75ec9faa88c28cac09e294494b10b
min1227 <evelyn.m.arellano@hotmail.com>
2024-04-15 00:57:22 -0500

    Broken animation WIP
    expect errors on opening. Working on errors.

--- KrazyCarts/Assets/Scripts/Animation MC.cs
+++ KrazyCarts/Assets/Scripts/Animation MC.cs
@@ -7,62 +7,78 @@ using UnityEngine;
 public class AnimationMC : MonoBehaviour
 {
 
-    public float walkSpeed = 7.5f;
-    public float runSpeed = 11.5f;
-
-    private bool isGrounded = true;
+    //public float walkSpeed = 7.5f;
+    //public float runSpeed = 11.5f;
+    
+   // private bool isGrounded = true;
     Rigidbody mC;
 
-    Animator animation;
+    private Animator animator;
+    private string currentAnimation = "";
 
-    bool isIdle = false;
-    bool cartCollected = false;
+   //bool isIdle = false;
+    //bool cartCollected = false;
 
     // Start is called before the first frame update
     void Start()
     {
         mC = GetComponent<Rigidbody>();
-        animation = GetComponent<Animator>();
-        cartCollected = false;
+        animator = GetComponent<Animator>();
+        //cartCollected = false;
+        changeAnimation("Walk");
     }
 
     // Update is called once per frame
     void FixedUpdate()
+
+        movement = new Vector2(Input.GetAxisRaw("Horizontal") Input.GetAxisRaw("Vertical"));
+    //{
+       // if (Input.GetKeyDown(KeyCode.W))
+       // {
+          //  if (isGrounded)
+           //     animator.Play("isWalking");
+           // else if (isGrounded)
+          //  {
+           //     cartCollected = true;
+           //     isIdle = false;
+           //     animator.Play("Pushing Cart Walk");
+           // }
+       // }//else if (Input.GetKeyDown(KeyCode.None))
+        //{
+         //   if (isGrounded && !isIdle)
+        //        animator.Play("Idle no cart");
+         //   else if (!isGrounded && !isIdle)
+            {
+        //        cartCollected = true;
+                //sisIdle = true;
+        //        animator.Play("Idle with cart");
+        //    }
+
+       // }
+      //  else if (Input.GetKeyDown(KeyCode.LeftShift))
+       // {
+         //   if (isGrounded)
+        //        animator.Play("isrunning");
+       //     else if (isGrounded)
+       //     {
+       //         cartCollected = true;
+       //         isIdle = false;
+       //         animator.Play("runWithCart");
+         //   }
+
+       // }
+
+    }
+    private void CheckAnimation()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            if (isGrounded)
-                animation.Play("isWalking");
-            else if (isGrounded)
-            {
-                cartCollected = true;
-                isIdle = false;
-                animation.Play("Pushing Cart Walk");
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.None))
-        {
-            if (isGrounded && !isIdle)
-                animation.Play("Idle no cart");
-            else if (!isGrounded && !isIdle)
-            {
-                cartCollected = true;
-                isIdle = true;
-                animation.Play("Idle with cart");
-            }
 
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftShift))
+    }
+    private void changeAnimation(string animation, float crossfade = 0.2f)
+    {
+        if (currentAnimation != animation)
         {
-            if (isGrounded)
-                animation.Play("isrunning");
-            else if (isGrounded)
-            {
-                cartCollected = true;
-                isIdle = false;
-                animation.Play("runWithCart");
-            }
-
+            currentAnimation = animation;
+            animator.CrossFade(animation, crossfade);
         }
 
     }
