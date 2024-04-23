eb19a18a124ceb6d15e59a57e82a6f734f34b749
Michael Morales <156836539+toxinfang@users.noreply.github.com>
2024-04-22 22:07:10 -0500

    WIP implement carts level 2

--- KrazyCarts/Assets/Scripts/CartPickUp.cs
+++ KrazyCarts/Assets/Scripts/CartPickUp.cs
@@ -21,11 +21,17 @@ public class CartPickUp : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        //To get your hinges to actually move, you'd need space and for these to not be confined in a collider for
+        //best results. I'd suggest putting separate colliders on each cart.
+
         m_Collider = GetComponent<BoxCollider>();
         m_ScaleX = 1;
         m_ScaleY = 1;
         m_ScaleZ = 1;
         //Find all GameObjects with Tag Player Carts and set them inactive
+        //WARNING. This DOES NOT FIND CHILDREN OBJECTS.
+        //If this isn't explicitly accessed anywhere, you could just Serialize it, make it private and drag them into the inspector.
+        //Or just drag them in the inspector, instead of using the Find method. It's less taxing on the system.
         GameObject[] _PlayerCarts = GameObject.FindGameObjectsWithTag("PlayerCarts");
         foreach (GameObject go in _PlayerCarts)
         {
@@ -43,8 +49,14 @@ public class CartPickUp : MonoBehaviour
         {
             Debug.Log("Touched");
             if (cartCount != _PlayerCarts.Length)
-            { 
+            {
+
                 _PlayerCarts[cartCount].SetActive(true);
+                if (cartCount > 0)
+                {
+                    //add the hinge joint in connecting it to the previous cart.
+                    _PlayerCarts[cartCount].GetComponent<HingeJoint>().connectedBody = _PlayerCarts[cartCount - 1].GetComponent<Rigidbody>(); ;
+                }
                 cartCount++;
             }
             ws.walkingSpeed *= 0.9f;
