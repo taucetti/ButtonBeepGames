723050c1c337fbaa8aa09e299934912c6f879efb
TrevorMinarikNVC <157077764+TrevorMinarikNVC@users.noreply.github.com>
2024-04-24 02:14:09 -0500

    Added skip button to intro cutscene

--- KrazyCarts/Assets/Scripts/BIL Cutscene Camera.cs
+++ KrazyCarts/Assets/Scripts/BIL Cutscene Camera.cs
@@ -6,22 +6,24 @@ using UnityEngine.Video;
 
 public class BILCutsceneCamera : MonoBehaviour
 {
-   
+
     VideoPlayer video;
 
-        void Awake()
-        {
-            video = GetComponent<VideoPlayer>();
-            video.Play();
-            video.loopPointReached += CheckOver;
-            GameObject camera = GameObject.Find("Main Camera");
-
-
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+        GameObject camera = GameObject.Find("Main Camera");
     }
 
-
-        void CheckOver(UnityEngine.Video.VideoPlayer vp)
-        {
-            SceneManager.LoadScene(3);//the scene that you want to load after the video has ended.
-        }
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        TransitionToNextScene();
     }
+
+    public void TransitionToNextScene()
+    {
+        SceneManager.LoadScene(3);//the scene that you want to load after the video has ended.
+    }
+}
