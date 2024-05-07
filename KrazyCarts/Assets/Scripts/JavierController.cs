b7b8c2733ddcfab1895f8b9b3dea4337f2645475
TrevorMinarikNVC <157077764+TrevorMinarikNVC@users.noreply.github.com>
2024-05-06 22:40:54 -0500

    Partial implementation of Javiar animations

---
+++ KrazyCarts/Assets/Scripts/JavierController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JavierController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.ResetTrigger("MCinCollider");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetTrigger("MCinCollider");
+    }
+}
