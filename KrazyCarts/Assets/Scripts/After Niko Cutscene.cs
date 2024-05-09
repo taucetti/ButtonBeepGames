0579177bb98932b3708c68d8188aab036ddb642d
min1227 <evelyn.m.arellano@hotmail.com>
2024-05-09 12:17:30 -0500

    Niko Cutscene

---
+++ KrazyCarts/Assets/Scripts/After Niko Cutscene.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class AfterNikoCutscene : MonoBehaviour
+{
+    VideoPlayer video;
+    //public AudioSource cutsceneAudio;
+
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+        GameObject camera = GameObject.Find("Main Camera");
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        TransitionToNextScene();
+    }
+
+    public void TransitionToNextScene()
+    {
+        //cutsceneAudio.Pause();
+        SceneManager.LoadScene(10);//the scene that you want to load after the video has ended.
+    }
+}
